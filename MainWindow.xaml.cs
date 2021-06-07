17b3a4cbb82104e934bfe5802c51ac1b46dbbf73
Ahmedphd1 <74898656+Ahmedphd1@users.noreply.github.com>
2021-06-07 13:11:23 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace wpfapp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        int accountype = 0; // 0 = normal: 1 = admin: 2 = super
+
+        public MainWindow()
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) // event handler for button - insert into database
+        {
+            if (accountype == 1 || accountype == 2)
+            {
+            }
+            else
+            {
+                MessageBox.Show("You need to be an admin or superuser to do that");
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e) // event handler for textbox
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) // event handler for button - deletes from the sql database
+        {
+
+            if (accountype == 1)
+            {
+                String query = "DELETE FROM fødselsrate_2017 WHERE ID = '" + Convert.ToInt32(combobox.SelectedItem.ToString()) + "'";
+
+            } else
+            {
+                MessageBox.Show("You need to be an admin to do that");
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e) // event handler for button - update sql database
+        {
+
+            if (accountype == 1 || accountype == 2)
+            {
+                String query = "UPDATE fødselsrate_2017 SET rang = '" + boxedit.Text + "' " + "WHERE ID = '" + Convert.ToInt32(combobox.SelectedItem.ToString()) + "'";
+
+            }
+            else
+            {
+                MessageBox.Show("You need to be an admin or superuser to do that");
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e) // event handler for button - authenticate login
+        {
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e) // button handler for button - display data from sql database
+        {
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e) // button handler for opret bruger fane - opretter bruger i databasen
+        {
+
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e) // button handler for nulstil kodeord fane - nulstiller kodeord for den valgte brugernavn
+        {
+;
+        }
+    }
+}
---
+++ sqlconn.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+
+namespace wpfapp
+{
+    class sqlconn
+    {
+
+        private string conn; // field
+
+        private SqlConnection connobject;
+
+        public sqlconn(string sqlcon)
+        {
+            conn = sqlcon; // Set the initial value for model
+        }
+
+        public void sqlconnect() // connect to sql function
+        {
+            SqlConnection cnn;
+            string connetionString = String.Format(@"{0}", conn);
+
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+
+            connobject = cnn;
+        }
+
+        public void insertcommand(string query, string land, string verdensdel1, string verdensdel2, int rang, int fødselsrate) // function to insert values to sql
+        {
+            SqlCommand insertCommand = new SqlCommand(query, connobject);
+
+            insertCommand.Parameters.Add(new SqlParameter("0", land));
+            insertCommand.Parameters.Add(new SqlParameter("1", verdensdel1));
+            insertCommand.Parameters.Add(new SqlParameter("2", verdensdel2));
+            insertCommand.Parameters.Add(new SqlParameter("3", rang));
+            insertCommand.Parameters.Add(new SqlParameter("3", fødselsrate));
+
+            insertCommand.ExecuteNonQuery();
+        }
+
+        public void insertlogin(string query, string navn, string kodeord, string rolle) // function to insert username into the database
+        {
+            SqlCommand command = new SqlCommand(query, connobject);
+
+            command.Parameters.Add(new SqlParameter("0", navn));
+            command.Parameters.Add(new SqlParameter("1", kodeord));
+            command.Parameters.Add(new SqlParameter("2", rolle));
+
+            command.ExecuteNonQuery();
+        }
+
+        public void editcommand(string query) // function to edit the sql server
+        {
+            SqlCommand command = new SqlCommand(query, connobject);
+
+            command.ExecuteNonQuery();
+        }
+
+        public void deletecommand(string query)
+        {
+            SqlCommand command = new SqlCommand(query, connobject);
+
+            command.ExecuteNonQuery();
+
+        }
+
+        public List<string> picktitles() // select ID column from sql database and return the values.
+        {
+            SqlCommand command = new SqlCommand("SELECT ID FROM fødselsrate_2017", connobject);
+
+            List<string> mylist = new List<string>();
+
+            command.Parameters.Add(new SqlParameter("0", 1));
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        mylist.Add(reader[i].ToString());
+                    }
+                }
+
+            }
+            return mylist;
+        }
+
+        public string authenticate(string brugernavn, string password) // function that authenticate login - select the specified role and login information to authenticate
+        {
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT COUNT(*) FROM login WHERE brugernavn='" + brugernavn + "' AND kodeord='" + password + "'", conn);
+
+            DataTable dt = new DataTable();
+
+            string returnValue = "";
+
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+
+                string query = "SELECT role from login WHERE brugernavn = @username and kodeord=@password";
+
+                using (SqlCommand sqlcmd = new SqlCommand(query, connobject))
+                {
+                    sqlcmd.Parameters.Add("@username", SqlDbType.VarChar).Value = brugernavn;
+                    sqlcmd.Parameters.Add("@password", SqlDbType.VarChar).Value = password;
+                    returnValue = (string)sqlcmd.ExecuteScalar();
+                }
+
+                return returnValue.Trim();
+            }
+            else
+                return "invalid";
+        }
+
+        public List<Dictionary<string, string>> selecteverything() // function to select everything from sql database and pass them to a list. 
+        {
+
+            Dictionary<string, string> column;
+
+            SqlCommand command = new SqlCommand("SELECT * FROM fødselsrate_2017", connobject);
+
+            List<Dictionary<string, string>> rows = new List<Dictionary<string, string>>();
+
+            command.Parameters.Add(new SqlParameter("0", 1));
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {    //Every new row will create a new dictionary that holds the columns
+                column = new Dictionary<string, string>();
+
+                column["ID"] = reader["ID"].ToString();
+                column["land"] = reader["land"].ToString();
+                column["verdensdel1"] = reader["verdensdel1"].ToString();
+                column["verdensdel2"] = reader["verdensdel2"].ToString();
+                column["rang"] = reader["rang"].ToString();
+                column["fødselsrate"] = reader["fødselsrate"].ToString();
+
+                rows.Add(column); //Place the dictionary into the list
+            }
+
+            return rows;
+        }
+
+
+    }
+}
